21a25fe94691c4a36bfa36450c2392c8ab53ba81
Cindy Colucy <ckcolucy@gmail.com>
2021-02-28 12:41:45 -0500

    Strings

---
+++ Strings/src/com/colucy/Main.java
@@ -0,0 +1,22 @@
+package com.colucy;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String myString = "This is a string";
+        System.out.println(myString);
+        myString = myString + ", and this is more.";
+        System.out.println(myString);
+        myString = myString + " \u00A9 2021";
+        System.out.println(myString);
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println(numberString);
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println(lastString);
+
+    }
+}
