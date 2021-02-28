7c44bec4ba6644847bd8fb3b5a528ad01c26bb0c
Cindy Colucy <ckcolucy@gmail.com>
2021-02-28 12:19:37 -0500

    Char and Boolean

---
+++ CharAndBoolean/src/com/colucy/Main.java
@@ -0,0 +1,17 @@
+package com.colucy;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+        System.out.println("My char = " + myChar);
+        System.out.println("My unicode char = " + myUnicodeChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+
+        boolean isCustomerOverTwentyOne = true;
+    }
+}
