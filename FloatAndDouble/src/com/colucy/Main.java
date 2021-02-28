db02eae31685f1fb2cd58f2310888208e392f0e5
Cindy Colucy <ckcolucy@gmail.com>
2021-02-28 11:58:53 -0500

    Float and Double

---
+++ FloatAndDouble/src/com/colucy/Main.java
@@ -0,0 +1,32 @@
+package com.colucy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float Minimum Value = " + myMinFloatValue);
+        System.out.println("Float Maximum Value = " + myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double Minimum Value = " + myMinDoubleValue);
+        System.out.println("Double Maximum Value = " + myMaxDoubleValue);
+
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5F / 3F;
+        double myDoubleValue = 5.00 / 3.00;
+        System.out.println("My int value = " + myIntValue);
+        System.out.println("My float value = " + myFloatValue);
+        System.out.println("My double value = " + myDoubleValue);
+
+        //challenge
+        double pounds = 5.0;
+        double result = pounds * 0.45359237;
+        System.out.println("Kilograms = " + result);
+
+
+
+
+    }
+}
