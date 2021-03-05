09e0cfc1724e547d43c183800c735913ab8b3666
Cindy Colucy <ckcolucy@gmail.com>
2021-03-05 09:36:20 -0500

    Keywords and Expressions

---
+++ KeywordsAndExpressions/src/com/colucy/Main.java
@@ -0,0 +1,24 @@
+package com.colucy;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // a  mile is equal to 1.609344 km
+        double kilometers = (100 * 1.609344);
+
+        int highScore = 50;
+        if (highScore== 50){
+            System.out.println("This is an expression.");
+
+        }
+
+        // Challenge
+        //score = 100
+        //if statement
+        //println
+        //score = 0;
+        
+
+    }
+}
