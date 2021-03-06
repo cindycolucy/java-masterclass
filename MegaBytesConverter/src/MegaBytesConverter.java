599112f3eca48f45454c79731cf2ff8292616112
Cindy Colucy <ckcolucy@gmail.com>
2021-03-06 10:43:00 -0500

    Mega Bytes Converter

---
+++ MegaBytesConverter/src/MegaBytesConverter.java
@@ -0,0 +1,13 @@
+public class MegaBytesConverter {
+
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes >= 0) {
+            int megaBytes = kiloBytes / 1024;
+            int remainder = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + remainder + " KB");
+
+        } else {
+            System.out.println("Invalid Value");
+        }
+    }
+}
