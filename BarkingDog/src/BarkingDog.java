31a63c1520071de9a704c0cf2c1c28079c17f18b
Cindy Colucy <ckcolucy@gmail.com>
2021-03-07 09:42:02 -0500

    Leap Year Calculator and Fixed Barking Dog

--- BarkingDog/src/BarkingDog.java
+++ BarkingDog/src/BarkingDog.java
@@ -1,11 +1,11 @@
 public class BarkingDog {
     public static void main(String[] args) {
-        shouldWakeUp(true, 9);
+        shouldWakeUp(false, 6);
     }
 
     public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
         if ((hourOfDay < 0 || hourOfDay > 23)
-                || (!barking && (hourOfDay > 8 && hourOfDay < 22))) {
+                || ((!barking) || (hourOfDay > 8 && hourOfDay < 22))) {
             System.out.println("False");
             return false;
         }
