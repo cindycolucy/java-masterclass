cb9c2edffa6b499ab8219574d949edaeaf21012a
Cindy Colucy <ckcolucy@gmail.com>
2021-03-27 14:46:06 -0400

    Input Calculator No Cheat

---
+++ InputCalculator/src/InputCalculator.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class InputCalculator {
+
+    public static void main(String[] args) {
+        inputThenPrintSumAndAverage();
+    }
+
+    public static void inputThenPrintSumAndAverage(){
+        Scanner scanner = new Scanner(System.in);
+
+        int sum = 0;
+        long average = 0;
+        int count = 0;
+
+        while(true){
+            boolean isAnInt = scanner.hasNextInt();
+
+            if(isAnInt){
+                int number = scanner.nextInt();
+                count++;
+                sum += number;
+                average = Math.round((double)sum / count);
+                scanner.nextLine();
+            } else {
+                break;
+            }
+
+        }
+        System.out.println("SUM = " + sum + " AVG = " + average);
+        scanner.close();
+    }
+}
