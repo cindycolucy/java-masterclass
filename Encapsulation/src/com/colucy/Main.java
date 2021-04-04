5e37d8767785880cd6167136e76c4420fc141d38
Cindy Colucy <ckcolucy@gmail.com>
2021-04-04 16:12:33 -0400

    Encapsulation

---
+++ Encapsulation/src/com/colucy/Main.java
@@ -0,0 +1,10 @@
+package com.colucy;
+
+public class Main {
+
+    public static void main(String[] args) {
+	EnhancedPlayer player = new EnhancedPlayer("Atticus", 50, "Sword");
+        System.out.println("Initial health is " + player.getHitpoints());
+
+    }
+}
